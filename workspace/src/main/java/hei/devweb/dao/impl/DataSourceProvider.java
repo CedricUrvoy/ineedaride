0f34f27593b284054e10e24585c336cb7417d956
CedricUrvoy <cedric.urvoy@hei.fr>
2013-12-18 15:03:43 +0100

    Creation de la connexion

---
+++ workspace/src/main/java/hei/devweb/dao/impl/DataSourceProvider.java
@@ -0,0 +1,25 @@
+package hei.devweb.dao.impl;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class DataSourceProvider {
+	
+	private static MysqlDataSource dataSource;
+
+	public static MysqlDataSource getDataSource() {
+		if (dataSource==null){
+			dataSource = new MysqlDataSource();
+			dataSource.setServerName("localhost");
+			dataSource.setPort(3306);
+			dataSource.setDatabaseName("tp03");
+			dataSource.setUser("root");
+			dataSource.setPassword("");
+		}
+		return dataSource;
+	}
+
+	public static void setDataSource(MysqlDataSource dataSource) {
+		DataSourceProvider.dataSource = dataSource;
+	}
+	
+}
