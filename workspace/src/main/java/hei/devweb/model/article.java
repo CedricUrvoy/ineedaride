a0798bd01e76b59e0cb84390cd3f37c26879ca86
CedricUrvoy <cedric.urvoy@hei.fr>
2014-01-09 18:43:08 +0100

    Article : Constructeur

--- workspace/src/main/java/hei/devweb/model/article.java
+++ workspace/src/main/java/hei/devweb/model/article.java
@@ -1,11 +1,20 @@
 package hei.devweb.model;
 
 public class article {
+	
 	private Integer id;
 	private String title;
 	private String description;
 	
 	private Integer idAlbum;
 	private Integer idCategorie;
+	
+	public article(Integer id, String title, String description, Integer idAlbum, Integer idCategorie) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.idAlbum = idAlbum;
+		this.idCategorie = idCategorie;
+	}
 
 }
