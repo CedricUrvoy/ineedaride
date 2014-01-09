e38b48a19c88e864000ee368bde4c646eed67c4e
CedricUrvoy <cedric.urvoy@hei.fr>
2014-01-09 18:48:12 +0100

    Article : Commentaires & Getters/Setters

--- workspace/src/main/java/hei/devweb/model/article.java
+++ workspace/src/main/java/hei/devweb/model/article.java
@@ -2,12 +2,16 @@ package hei.devweb.model;
 
 public class article {
 	
+	/**** Declaration des variables ****/
+	
 	private Integer id;
 	private String title;
 	private String description;
 	
 	private Integer idAlbum;
 	private Integer idCategorie;
+	
+	/**** Contructeur(s) ****/
 	
 	public article(Integer id, String title, String description, Integer idAlbum, Integer idCategorie) {
 		this.id = id;
@@ -16,5 +20,51 @@ public class article {
 		this.idAlbum = idAlbum;
 		this.idCategorie = idCategorie;
 	}
+	
+	/**** Methode(s) ****/
+	
+	
+	/**** Getters and Setters ****/
+	
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getIdAlbum() {
+		return idAlbum;
+	}
+
+	public void setIdAlbum(Integer idAlbum) {
+		this.idAlbum = idAlbum;
+	}
+
+	public Integer getIdCategorie() {
+		return idCategorie;
+	}
+
+	public void setIdCategorie(Integer idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+
+	
 }
