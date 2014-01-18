255890e5e27f7a21b9dfc41fe759852e2513addf
CedricUrvoy <cedric.urvoy@hei.fr>
2014-01-18 15:45:43 +0100

    ArticlesServlet : Creation

---
+++ workspace/src/main/java/hei/devweb/controllers/ArticlesServlet.java
@@ -0,0 +1,9 @@
+package hei.devweb.controllers;
+
+import javax.servlet.http.HttpServlet;
+
+public class ArticlesServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -6601439810866984722L;
+
+}
